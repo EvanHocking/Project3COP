809c19e2d4902f7c5a51033d7ff4e5db3c977836
Katrina Beaucage <kbeaucage@ufl.edu>
2020-11-23 19:49:06 -0500

    This is for reading the database and putting it in a vector of vectors

---
+++ DatabaseVector.cpp
@@ -0,0 +1,34 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <string>
+using namespace std;
+
+void Read_Database() {
+	//File pointer
+	fstream file;
+
+	//open existing file
+	file.open("Database.csv", ios::in);
+
+	//read data from file
+	//store it in a vector of vectors
+	vector<vector<string>> storage;
+	vector<string> row;
+	string line, word, temp;
+
+	while (file >> temp) {
+
+		row.clear();
+		//read a row
+		getline(file, line);
+		stringstream s(line);
+		//read column data and store in word
+		while (getline(s, word, ',')) {
+			row.push_back(word);
+		}
+		storage.push_back(row);
+
+	}
+}
