691af38d314b3d840f65a9504ce3b37fa11ff18b
Toni Earle <tverandell@gmail.com>
2020-12-02 01:46:00 -0500

    finalized organizeList

--- toni.cpp
+++ toni.cpp
@@ -11,36 +11,45 @@ public:
     int aisleNum;
     string itemName;
     string itemCategory;
+    string brandName;
 
-    Item(int aisle, string name, string category)
+    Item(int aisle, string name, string category, string brand)
     {
         aisleNum = aisle;
         itemName = name;
         itemCategory = category;
+        brandName = brand;
     }
-}
+};
 
 class GroceryMap
 {
 private:
-    map<int, vector<Item>> grocery;
+    map<int, vector<Item*>> grocery;
 public:
-    void addEdge(int aisle, string name, string category);
-}
+    void addEdge(int aisle, string name, string category, string brand);
+    void organizeList();
+};
 
-void GroceryMap::addEdge(int aisle, string name, string category)
+void GroceryMap::addEdge(int aisle, string name, string category, string brand)
 {
-    Item newItem = new Item(aisle,name,category);
+    Item* newItem = new Item(aisle,name,category,brand);
     grocery[aisle].push_back(newItem);
 }
 
-
 void GroceryMap::organizeList()
 {
-    for(int i = 0; i < randomList.size; i++)
+    //organize and print list in format:
+    // Optimized List: Map
+    //========== AISLE 1 ==========
+    //brand name   |   item name
+    cout << "Optimized List: MAP" << endl;
+    for (map<int, vector<Item*>>::iterator it=grocery.begin(); it!=grocery.end(); ++it)
     {
-        //search in each aisle and each category to find item,
-        //create a new map of the new items and each aisle
-        //return map
+        cout << "========== AISLE "<< it->first << " ==========" << endl;
+        for (int i = 0; i < it->second.size(); i++)
+        {
+            cout << it->second[i]->brandName << " | " << it->second[i]->itemName << endl;
+        }
     }
 }
