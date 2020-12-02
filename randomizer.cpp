10d5088cf3df6cd77e552de9e5d567045137d359
Evan Hocking <evanhocking@ufl.edu>
2020-12-02 01:35:43 -0500

    my randomizer algorithm

---
+++ randomizer.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <string>
+#include <time.h>
+using namespace std;
+
+// data base structure, randomized list size, seeded?, seed number
+vector<vector<string>> randomizeList(vector<vector<string>>& _dataBase, int _listSize, bool _isSeeded, unsigned int _seed)
+{
+	vector<vector<string>> list;
+
+	//Set the seed if needed
+	if (_isSeeded) { srand(_seed); }
+	else { srand((unsigned)time(0)); }
+
+	for (int i = 0; i < _listSize; i++)
+	{
+		int randomNumber = rand() % _dataBase.size();		//generates a random number with a upperlimit of the size of the data base
+		list.push_back(_dataBase.at(randomNumber));			//using the random number, index list and add it to randomized list
+	}
+
+	return list;
+}
+
+int main()
+{
+	vector<vector<string>> test = Read_Database();
+	vector<vector<string>> randomizedList = randomizeList(test, 3, true, 1);
+
+	return 0;
+}
